dc4bd8dda825502b5bff64e792ded0b381f5b174
Daniel nix <daniel.nix@gmail.com>
2023-02-09 19:58:07 +0100

    v 0.2
    trying to catch errors when internet goes down (doesn't work)

--- stib2mqtt/stib2mqtt.py
+++ stib2mqtt/stib2mqtt.py
@@ -30,6 +30,31 @@ STOPS = configuration['stops']
 LANG = configuration['lang']
 MESSAGE_LANG = configuration['message_lang']
 
+def check_live(url):
+    #print(url)
+    try:
+        #print('try')
+        r = requests.get(url)
+        live = r.ok
+        #print(r.status_code)
+        if (r.status_code == 400):
+           live = True
+    except requests.ConnectionError as e:
+        print("ERROR")
+
+        print(e)
+        if 'MaxRetryError' not in str(e.args) or 'NewConnectionError' not in str(e.args):
+            raise
+        if "[Errno 8]" in str(e) or "[Errno 11001]" in str(e) or ["Errno -2"] in str(e):
+            print('DNSLookupError')
+            live = False
+        else:
+            raise
+    except:
+        raise
+    #print(live)
+    return live
+
 def getStibData(q, dataset):
     url = STIB_API
     params = dict(
@@ -39,13 +64,25 @@ def getStibData(q, dataset):
     rows=99,
     apikey = STIB_API_KEY
     )
-    data = False
+    data = []
     try:
         r = requests.get(url=url, params=params)
-        r.raise_for_status()
         data = r.json()
-    except requests.exceptions.HTTPError as e:
-        print (e.response.text)
+        live = r.ok
+    except requests.ConnectionError as e:
+
+        print("ERROR in StibData")
+        if 'MaxRetryError' not in str(e.args) or 'NewConnectionError' not in str(e.args):
+            print("MaxRetry")
+            raise
+        if "[Errno 8]" in str(e) or "[Errno 11001]" in str(e) or "[Errno -2]" or "[Errno 3]" in str(e):
+            print('DNSLookupError')
+            live = False
+        else:
+            raise
+    except:
+        raise
+    #print(live)
     return data
 
 def getStopInfos():
@@ -56,11 +93,11 @@ def getStopInfos():
     RouteFields = {}
     for stopId, stop in enumerate(STOPS):
         stopIds.append(stop['stop_id'])
-        k = "STOP"+str(stop['stop_id']) 
+        k = "STOP"+str(stop['stop_id'])
         StopFields[k] = {"stop_id":stop['stop_id']}
         for line_id in stop['line_numbers']:
             if line_id not in lineIds:
-                keyName= "L" + str(line_id) 
+                keyName= "L" + str(line_id)
                 LineFields[keyName]={"line_id":line_id}
                 RouteFields[keyName]={"line_id":line_id}
                 lineIds.append(line_id)
@@ -68,7 +105,7 @@ def getStopInfos():
     q = " OR ".join(str(item) for item in stopIds)
     dataset='stop-details-production',
     StopData = getStibData(q, dataset)
-    if "records" in StopData:
+    if StopData and "records" in StopData:
         StopRecords = StopData['records']
     else:
         StopRecords = False
@@ -76,11 +113,12 @@ def getStopInfos():
         for r in StopRecords:
             stopsName  = json.loads(r["fields"]["name"])
             stopId = json.loads(r["fields"]["id"])
-            gpscoordinates = json.loads(r["fields"]['gpscoordinates']) 
+            gpscoordinates = json.loads(r["fields"]['gpscoordinates'])
             k = "STOP" + str(stopId)
             StopFields[k]["stop_names"] = stopsName
-            StopFields[k]["gps_coordinates"] = gpscoordinates 
-            
+            StopFields[k]["gps_coordinates"] = gpscoordinates
+    else: return []
+
     q = " OR ".join(str(item) for item in lineIds)
     dataset='stops-by-line-production'
     StopData = getStibData(q, dataset)
@@ -96,11 +134,11 @@ def getStopInfos():
                 destination = (r["fields"]["destination"])
                 direction = (r["fields"]["direction"])
                 LineFields[k]["destination"] = destination
-                LineFields[k]["direction"] = direction 
+                LineFields[k]["direction"] = direction
 
     dataset='gtfs-routes-production'
     RoutesData = getStibData(q, dataset)
-    if "records" in RoutesData:  
+    if "records" in RoutesData:
         RoutesRecords = RoutesData['records']
     else:
         RoutesRecords = False
@@ -111,28 +149,32 @@ def getStopInfos():
             if k in LineFields:
                 RouteFields[k]["route_type"] = r['fields']['route_type']
                 RouteFields[k]["route_color"] = r['fields']['route_color']
-        
-    return {"stops": StopFields, "lines": LineFields, "routes": RouteFields}    
+
+    return {"stops": StopFields, "lines": LineFields, "routes": RouteFields}
 
 
 def getWaitingTimes(fields):
+    #print(fields)
+    if (len(fields) == 0 ):
+        return False
     now = pytz.utc.normalize(pytz.utc.localize(datetime.datetime.utcnow()))
     stopIds = []
     lineIds = []
     StopFields = fields['stops']
-    LineFields = fields['lines'] 
+    #print(StopFields)
+    LineFields = fields['lines']
     RouteFields = fields['routes']
     WaitingTimeFields = {}
     for stopId, stop in enumerate(STOPS):
         stopIds.append(stop['stop_id'])
-        k = str(stop['stop_id']) 
+        k = str(stop['stop_id'])
         for line_id in stop['line_numbers']:
-            keyName= "L" + str(line_id) 
+            keyName= "L" + str(line_id)
             stopName = StopFields['STOP' + k]['stop_names'][LANG]
             routeType = RouteFields[keyName]["route_type"]
             routeColor = RouteFields[keyName]["route_color"]
-            WaitingTimeFields[keyName + k + "1"]= {"arrival":0, "destination":"","gpscoordinates":"","message":"","status":"not available", "stopName":stopName, "timestamp":"", "vehicle_type":routeType, "route_color":routeColor, "end_of_service":True }
-            WaitingTimeFields[keyName + k + "2"]= {"arrival":0, "destination":"","gpscoordinates":"","message":"","status":"not available", "stopName":stopName, "timestamp":"", "vehicle_type":routeType, "route_color":routeColor, "end_of_service":True }
+            WaitingTimeFields[keyName + k + "1"]= {"arrival":0, "destination":"","gpscoordinates":"","message":"","status":"not available", "stopName":stopName, "timestamp":"", "vehicle_type":routeType, "route_color":routeColor, "end_of_service":True, 'line':str(line_id) }
+            WaitingTimeFields[keyName + k + "2"]= {"arrival":0, "destination":"","gpscoordinates":"","message":"","status":"not available", "stopName":stopName, "timestamp":"", "vehicle_type":routeType, "route_color":routeColor, "end_of_service":True, 'line':str(line_id) }
 
     q = " OR ".join(str(item) for item in stopIds)
     dataset='waiting-time-rt-production'
@@ -164,14 +206,15 @@ def getWaitingTimes(fields):
                 ttt = datetime.datetime.fromisoformat(t)
                 tmp = pytz.utc.normalize(ttt)
                 minutes = round( (tmp-now).total_seconds()/60)
+                WaitingTimeFields[lineId]["line"] = str(r["fields"]["lineid"])
                 WaitingTimeFields[lineId]["arrival"] = minutes
-                WaitingTimeFields[lineId]["timestamp"] = t 
-                WaitingTimeFields[lineId]["message"] = message 
-                WaitingTimeFields[lineId]["end_of_service"] = eos 
+                WaitingTimeFields[lineId]["timestamp"] = t
+                WaitingTimeFields[lineId]["message"] = message
+                WaitingTimeFields[lineId]["end_of_service"] = eos
                 WaitingTimeFields[lineId]["destination"] = destination
-                WaitingTimeFields[lineId]["status"] = av 
-                WaitingTimeFields[lineId]["stopName"] = StopFields['STOP'+str(pointId)]["stop_names"][LANG] 
-                WaitingTimeFields[lineId]["gpscoordinates"] = StopFields['STOP'+str(pointId)]["gps_coordinates"] 
+                WaitingTimeFields[lineId]["status"] = av
+                WaitingTimeFields[lineId]["stopName"] = StopFields['STOP'+str(pointId)]["stop_names"][LANG]
+                WaitingTimeFields[lineId]["gpscoordinates"] = StopFields['STOP'+str(pointId)]["gps_coordinates"]
                 if len(pt) > 1:
                     p = "2"
                     x = 1
@@ -191,20 +234,21 @@ def getWaitingTimes(fields):
                     ttt = datetime.datetime.fromisoformat(t)
                     tmp = pytz.utc.normalize(ttt)
                     minutes = round( (tmp-now).total_seconds()/60)
+                    WaitingTimeFields[lineId]["line"] = str(r["fields"]["lineid"])
                     WaitingTimeFields[lineId]["arrival"] = minutes
-                    WaitingTimeFields[lineId]["timestamp"] = t 
-                    WaitingTimeFields[lineId]["message"] = message 
+                    WaitingTimeFields[lineId]["timestamp"] = t
+                    WaitingTimeFields[lineId]["message"] = message
                     WaitingTimeFields[lineId]["destination"] = destination
-                    WaitingTimeFields[lineId]["end_of_service"] = eos 
-                    WaitingTimeFields[lineId]["status"] = av 
-                    WaitingTimeFields[lineId]["stopName"] = StopFields['STOP'+str(pointId)]["stop_names"][LANG] 
-                    WaitingTimeFields[lineId]["gpscoordinates"] = StopFields['STOP'+str(pointId)]["gps_coordinates"] 
-         
+                    WaitingTimeFields[lineId]["end_of_service"] = eos
+                    WaitingTimeFields[lineId]["status"] = av
+                    WaitingTimeFields[lineId]["stopName"] = StopFields['STOP'+str(pointId)]["stop_names"][LANG]
+                    WaitingTimeFields[lineId]["gpscoordinates"] = StopFields['STOP'+str(pointId)]["gps_coordinates"]
+
 
     #pprint.pprint(WaitingTimeFields)
     return WaitingTimeFields
 
-  
+
 
 
 def connect_mqtt():
@@ -226,25 +270,28 @@ def publish(client):
     lastDate = datetime.datetime.today()
     data = getStopInfos()
     while True:
-        msg = False 
+        msg = False
         if  checkUpdate(lastDate):
             data = getStopInfos()
             lastDate = datetime.datetime.today()
 
         msg = getWaitingTimes(data)
         if msg:
+            for x in msg:
+                 #  print(msg[x]["stopName"], ": ",msg[x]['line'],' ', msg[x]['arrival'],'min')
+                 pprint.pprint(msg[x])
             msg = json.dumps(msg, indent=4, sort_keys=True, ensure_ascii=False)
             result = client.publish(mqtt_topic, msg)
             status = result[0]
             if status == 0:
-                print(f"Send `{msg}` to topic `{mqtt_topic}`")
+                print(f"Send msg to topic `{mqtt_topic}`")
             else:
                 print(f"Failed to send message to topic {mqtt_topic}")
             msg_count += 1
             print(msg_count)
 
         # result: [0, 1]
-        
+
         print("sleep")
         time.sleep(20)
 
